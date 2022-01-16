90e82d876c277a35c9c636beb04380294d9b75f4
Joshua Mercado <mercado.joshua.work@gmail.com>
2022-01-16 16:43:13 +0800

    first message

---
+++ dist/js/behaviors/nav-section.js
@@ -0,0 +1,19 @@
+"use strict";
+
+document.addEventListener("readystatechange", function (event) {
+  if (event.target.readyState === "complete") {
+    init();
+  }
+});
+
+function init() {
+  var openBtn = document.querySelector("[data-js-nav-open]");
+  var closeBtn = document.querySelector("[data-js-nav-close]");
+  var navbar = document.querySelector("[data-js-nav-section]");
+  openBtn.addEventListener("click", function () {
+    navbar.classList.add("-active");
+  });
+  closeBtn.addEventListener("click", function () {
+    navbar.classList.remove("-active");
+  });
+}
